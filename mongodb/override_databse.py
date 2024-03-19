489db0926015916e278b7c8b343986935ef5ab11
acampos <alejandro.campos@sita.aero>
2024-03-19 14:14:23 +0100

    While control in override_databse.py

--- mongodb/override_databse.py
+++ mongodb/override_databse.py
@@ -1,15 +1,22 @@
-# import subprocess
+import subprocess
 
-# import utilities as ut
+import utilities as ut
 
-# # Command to copy the directory
-# command = "rm -rf /var/lib/docker/volumes/b-tool-dcompose_mongodb_data && sudo cp -r ./backup-db /var/lib/docker/volumes/b-tool-dcompose_mongodb_data"
+# Command to copy the directory
+command = "rm -rf /var/lib/docker/volumes/b-tool-dcompose_mongodb_data && sudo cp -r ./backup-db /var/lib/docker/volumes/b-tool-dcompose_mongodb_data"
 
-# # Execute the command
-# result = subprocess.run(command, shell=True)
+control = ''
+while control.lower() != 'override-database':
+    control = input(f"{ut.bcolors.WARNING}Are you sure do you want to override the database? This action will be irreversible\nType 'override-database' to continue: / 'n' to abort: {ut.bcolors.ENDC}")
+    if control.lower() == 'n':
+        print(f"{ut.bcolors.WARNING}Exiting the execution{ut.bcolors.ENDC}")
+        exit(1)
 
-# # Check if the command executed successfully
-# if result.returncode == 0:
-#     print(f"{ut.bcolors.OKGREEN}Databse backed up successfully on /home/acampos/b-tool-dcompose_mongodb_data{ut.bcolors.ENDC}")
-# else:
-#     print(f"{ut.bcolors.FAIL}Something went wrong backing up the database in /home/acampos/b-tool-dcompose_mongodb_data\n{result}{ut.bcolors.ENDC}")
+# Execute the command
+result = subprocess.run(command, shell=True)
+
+# Check if the command executed successfully
+if result.returncode == 0:
+    print(f"{ut.bcolors.OKGREEN}Databse backed up successfully on /home/acampos/b-tool-dcompose_mongodb_data{ut.bcolors.ENDC}")
+else:
+    print(f"{ut.bcolors.FAIL}Something went wrong backing up the database in /home/acampos/b-tool-dcompose_mongodb_data\n{result}{ut.bcolors.ENDC}")
--- mongodb/tests.py
+++ mongodb/tests.py
@@ -1,31 +1,31 @@
-for doc in collection.find():
-    # Extract the month from the birthday field
-    month = doc['birthday'].month
+# for doc in collection.find():
+#     # Extract the month from the birthday field
+#     month = doc['birthday'].month
     
-    if month == 1:
-        month = 'january'
-    elif month == 2:
-        month = 'february'
-    elif month == 3:
-        month = 'march'
-    elif month == 4:
-        month = 'april'
-    elif month == 5:
-        month = 'may'
-    elif month == 6:
-        month = 'june'
-    elif month == 7:
-        month = 'july'
-    elif month == 8:
-        month = 'august'
-    elif month == 9:
-        month = 'september'
-    elif month == 10:
-        month = 'october'
-    elif month == 11:
-        month = 'november'
-    else:
-        month = 'december'
+#     if month == 1:
+#         month = 'january'
+#     elif month == 2:
+#         month = 'february'
+#     elif month == 3:
+#         month = 'march'
+#     elif month == 4:
+#         month = 'april'
+#     elif month == 5:
+#         month = 'may'
+#     elif month == 6:
+#         month = 'june'
+#     elif month == 7:
+#         month = 'july'
+#     elif month == 8:
+#         month = 'august'
+#     elif month == 9:
+#         month = 'september'
+#     elif month == 10:
+#         month = 'october'
+#     elif month == 11:
+#         month = 'november'
+#     else:
+#         month = 'december'
     
     # Update the document in the collection
-    collection.update_one({'_id': doc['_id']}, {'$set': {'month': month}})
+    #collection.update_one({'_id': doc['_id']}, {'$set': {'month': month}})
