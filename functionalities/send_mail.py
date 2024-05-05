dd6b6557219b9891dec14d422c370b825f7f92a1
titocampis <andreasscorelli@gmail.com>
2024-05-05 17:31:13 +0200

    Refactored the send mail

--- functionalities/send_mail.py
+++ functionalities/send_mail.py
@@ -1,33 +1,30 @@
-from flask import Flask, render_template, request, jsonify
+from flask import jsonify
 from flask_mail import Mail, Message
 
-def send_email(sender_mail_username):
+def send_email(sender_mail_username, receiver_mail_username, subject, data_name, data_birthday, data_sex):
     try:
-        # Retrieve data from form
-        data = request.get_json()
-        
-        # Define Variables
-        name = str(data['name'])
-        email = str(data['email'])
-        subject = str(data['subject'])
-        message = str(data['message'])
 
         # Check if any required field is empty
-        if not name or not email or not subject or not message:
+        if not subject or not data_name or not data_birthday or not data_sex:
             raise ValueError('Missing required fields')
 
-        # Validate email format
-        if '@' not in email:
-            raise ValueError('Invalid email format')
+        # Validate email formats
+        if '@' not in sender_mail_username:
+            raise ValueError('Invalid sender email format')
+        if '@' not in receiver_mail_username:
+            raise ValueError('Invalid receiver email format')
+        
+        # Calculate years
+        # years =
         
         # Define the message to send
         mail_message = Message(
                 subject = subject, 
-                sender =  (name, sender_mail_username), 
-                recipients = ['andreasscorelli@gmail.com'])
+                sender =  ("B-Tool Bot", sender_mail_username), 
+                recipients = [str(receiver_mail_username)])
         
         # Define body
-        mail_message.body = f"Nombre: {name}\nMail: {email}\n{message}"
+        mail_message.body = f"Muchísimas felicidades {data_name}!! Espero que pases un muy feliz {years} día de cumpleaños, un fuerte abrazo <3"
         
         # Send the email
         # mail.send(mail_message)
