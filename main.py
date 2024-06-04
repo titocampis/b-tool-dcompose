80435af6312670fd26710ba357b3fcd1b1506a9c
titocampis <andreasscorelli@gmail.com>
2024-06-04 22:10:43 +0200

    Pupurri successfully working

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ from utils import send_mail
 # mh.main_horoscope()
 # mm.main_mongodb()
 
-# send_mail.send_mail('alex@','clau@','test','clau',datetime(1998,8,8), False)
+sender_mail_username = ''
+sender_mail_password = ''
+receiver_mail_username = ''
+subject = ''
+
+print(send_mail.send_mail(sender_mail_username, sender_mail_password, 'B-Tool Boot', receiver_mail_username, subject, datetime(1998,8,8)))
--- utils/send_mail.py
+++ utils/send_mail.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from flask import jsonify
-from flask_mail import Mail, Message
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 import utils.utilities as mut
 
-def send_mail(sender_mail_username:str, receiver_mail_username:str, subject:str, data_alias:str, data_birthday:datetime, data_sex:bool):
+def send_mail(sender_mail_username:str, sender_mail_password:str, sender_name:str, receiver_mail_username:str, subject:str, data_birthday:datetime):
+    '''Method to send a mail'''
     try:
 
         # Validate email formats
@@ -16,16 +20,32 @@ def send_mail(sender_mail_username:str, receiver_mail_username:str, subject:str,
         # Calculate years
         years = mut.calculate_old(data_birthday)
         
-        # Define the message to send
-        mail_message = Message(
-                subject = subject, 
-                sender =  ("B-Tool Bot", sender_mail_username), 
-                recipients = [str(receiver_mail_username)])
-        
-        # Define body
-        mail_message.body = f"Muchísimas felicidades {data_alias}!! Espero que pases un gran día, disfruta mucho de tus {years}.\nFeliz cumpleaños y un fuerte abrazo! <3"
+        # Create a MIMEText object to represent the email
+        message = MIMEMultipart()
 
-        mail.send(mail_message)
+        # Define the message to send
+        message["From"] = sender_mail_username
+        message["To"] = receiver_mail_username
+        message["Subject"] = "Test email from Python"
+        
+        # Define & attach body
+        body = f"Muchísimas felicidades!! Espero que pases un gran día, disfruta mucho de tus {years}.\nFeliz cumpleaños y un fuerte abrazo! <3"
+        message.attach(MIMEText(body, "plain"))
+
+        # Connect to the SMTP server (in this case, Gmail's SMTP server)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+
+        # Login to your email account
+        server.login(sender_mail_username, sender_mail_password)
+
+        # Send the email
+        server.sendmail(sender_mail_username, receiver_mail_username, message.as_string())
+
+        # Quit the SMTP server
+        server.quit()
+
+        print("Email sent successfully!")
 
         # return jsonify({'message': 'Email sent successfully'}), 200
     
